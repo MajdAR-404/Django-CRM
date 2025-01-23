9e80e46a9e612b20a7f4cde7f21a988a503efd42
Hubo10 <bozdemir.huseyini@gmail.com>
2025-01-23 10:53:44 +0100

    django codes updated

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+ 
 # Execute startup scripts
 ./wait-for-postgres.sh "$DBHOST"
 python3 manage.py collectstatic --noinput
+python3 manage.py makemigrations
 python3 manage.py migrate
+python3 manage.py shell < seeders/seed.py
 python3 manage.py runserver 0.0.0.0:8000
